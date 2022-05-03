702a71881b95dc7a46083c080f4089a4ad5e50ce
Robson Manoel Bento Teixeira <robsonmteixeira@outlook.com>
2022-05-03 00:44:22 -0300

    Tela de Cadastro

--- petshop/src/app/app.module.ts
+++ petshop/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginPageComponent,
-    ResetPasswordPageComponent
+    ResetPasswordPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
