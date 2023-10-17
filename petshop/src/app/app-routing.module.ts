4632494a29dba074ac30288dabe675f8d3502afd
Robson Manoel Bento Teixeira <robsonmteixeira@outlook.com>
2023-10-17 00:59:05 -0300

    Route Guard

--- petshop/src/app/app-routing.module.ts
+++ petshop/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { FramePageComponent } from './pages/master/frame.page';
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { AuthService } from './services/auth.service';
 
 const routes: Routes = [
   {
@@ -14,12 +15,13 @@ const routes: Routes = [
     component: FramePageComponent,
     children: [
       { path: '', component: ProductsPageComponent },
-      { path: 'cart', component: CartPageComponent }
+      { path: 'cart', component: CartPageComponent, canActivate: [AuthService] }
     ]
   },
   {
     path: 'account',
     component: FramePageComponent,
+    canActivate: [AuthService],
     children: [
       { path: 'pets', component: PetsPageComponent }
     ]
