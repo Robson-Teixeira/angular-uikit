4555dad18e9ba518ef5ade6f1cc4e8ecb2a8f23d
Robson Manoel Bento Teixeira <robsonmteixeira@outlook.com>
2023-10-21 23:58:06 -0300

    Restauração de Senha

--- petshop/src/app/pages/account/reset-password-page/reset-password-page.component.ts
+++ petshop/src/app/pages/account/reset-password-page/reset-password-page.component.ts
@@ -1,14 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
+import { CustomValidator } from 'src/app/validators/custom.validators';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-reset-password-page',
   templateUrl: './reset-password-page.component.html'
 })
 export class ResetPasswordPageComponent implements OnInit {
+  
+  public form: FormGroup;
+  public busy = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private service: DataService,
+    private fb: FormBuilder,
+    private toastr: ToastrService
+  ) {
+    this.form = this.fb.group({
+      document: ['', Validators.compose([
+        Validators.minLength(14),
+        Validators.maxLength(14),
+        Validators.required,
+        CustomValidator.isCpf()
+      ])]
+    });
   }
 
+  ngOnInit() {
+  }
+
+  submit() {
+    this.busy = true;
+    this
+      .service
+      .resetPassword(this.form.value)
+      .subscribe(
+        {
+          next: (data: any) => {
+            this.toastr.success(data.message, 'Senha Restaurada');
+            this.router.navigate(['/login']);
+          },
+          error: (err: any) => console.log(err),
+          complete: () => this.busy = false
+        }
+      );
+  }
 }
