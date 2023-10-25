8da63849737a7f61d56e3bda8ad6ce8355e0a700
Robson Manoel Bento Teixeira <robsonmteixeira@outlook.com>
2023-10-25 01:24:47 -0300

    Quantidade de Itens no Carrinho

--- petshop/src/app/components/shared/navbar/navbar.component.ts
+++ petshop/src/app/components/shared/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { Security } from 'src/app/utils/security.util';
+import { Cart } from 'src/app/models/cart.model';
+import { CartUtil } from 'src/app/utils/cart.util';
 
 @Component({
   selector: 'app-navbar',
@@ -10,11 +12,23 @@ import { Security } from 'src/app/utils/security.util';
 export class NavbarComponent implements OnInit {
   
   public user!: User | null;
-
+  
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.user = Security.getUser();
+  }
+
+  public countQuantity() {
+    
+    let cart: Cart = CartUtil.get();
+    let count: number = 0;
+
+    cart.items.forEach((item) => {
+      count += item.quantity;
+    });
+    
+    return count;
   }
 
   logout() {
